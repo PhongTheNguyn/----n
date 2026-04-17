4aefce167b117824ffcfe12f163b467535c431c2
NguyenThePhong-2121051045 <2121051045@student.humg.edu.vn>
2026-04-17 15:13:06 +0700

    ud mobile ui và bỏ giới tính khác

--- Front_end/src/app/pages/profile/profile.component.ts
+++ Front_end/src/app/pages/profile/profile.component.ts
@@ -20,8 +20,7 @@ export class ProfileComponent implements OnInit {
 
   genders = [
     { value: 'male', label: 'Nam' },
-    { value: 'female', label: 'Nữ' },
-    { value: 'other', label: 'Khác' }
+    { value: 'female', label: 'Nữ' }
   ];
 
   countries = [
